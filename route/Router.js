1ec6b8e3d49538dcee6ce772192c2e5fad1ac8f0
Furkan <113936184+furkanzelik@users.noreply.github.com>
2023-12-10 20:56:26 +0100

    Add files via upload
    new

--- route/Router.js
+++ route/Router.js
@@ -1,32 +1,105 @@
 import express, {request, response} from "express";
-import Information from "../models/Information";
-import { faker } from '@faker-js/faker';
+import {faker, tr} from '@faker-js/faker';
+import Empire from "../models/empire.js";
+const routes = express.Router();
 
 
 
-const router = express.Router();
+    // get all data
+    routes.get('/', async (req,res) =>{
 
- route.get('/', async (request,response)=>{
+        try {
+            const empires = await Empire.find({});
+            res.json(empires);
+            console.log('working');
 
-     let information = await Information.find()
+        } catch (err){
+            console.error(err);
+            res.status(500).json({ error: 'Internal Server Error' });
+        }
+    });
 
-     res.json(information)
- })
 
-route.get('/id', async (request,response)=>{
-    try {
-        let info = await Information.findOne({'_id': req.params.id})
-        res.json(info)
-    } catch (e){
-        res.json({
-            message: 'Couldnt found info'
-        })
-    }
-})
+    // get data by id
+    routes.get('/:id', async (req, res) => {
+        try {
+            const { id } = req.params; // Fix the typo here
+            const empires = await Empire.findById(id);
+            res.json(empires);
+            console.log('working');
+        } catch (err) {
+            console.error(err);
+            res.status(500).json({ error: 'Internal Server Error' });
+        }
+    });
 
-route.post('/', async (req,res)=>{
 
-})
+// create new with faker
+    routes.post('/', async (req,res) =>{
 
-export default route
+        try {
+            const empires = await Empire.create({
+                civilization: faker.lorem.word({length:{min:3 , max:10}}),
+                specialty: faker.lorem.word({length:{min:3 , max:10}}),
+                army: faker.lorem.word({length:{min:3 , max:10}}),
+                difficulty: faker.lorem.word({length:{min:3 , max:10}})
 
+            });
+            res.json(empires);
+            console.log('seeder works');
+        } catch (err){
+            console.log(err);
+            res.status(500).json({error: 'Internal server error'})
+        }
+    });
+
+// update empire
+
+    routes.put('/:id', async (req, res) => {
+        try {
+            if (
+                !req.body.civilization ||
+                !req.body.specialty ||
+                !req.body.army ||
+                !req.body.difficulty
+            ) {
+                return res.status(400).send({
+                    message: 'Send all required fields: civilization, specialty, army, difficulty'
+                });
+            }
+
+            const { id } = req.params;
+            const result = await Empire.findByIdAndUpdate(id, req.body);
+
+            if (!result) {
+                return res.status(404).json({ message: 'Empire not found' });
+            }
+
+            res.json(result); // Send the updated document as the response
+        } catch (err) {
+            console.log(err);
+            res.status(500).json({ error: 'Internal server error' });
+        }
+    });
+
+// Delete empire
+    routes.delete('/:id', async (req,res) =>{
+        try {
+            const { id } = req.params;
+            const result = await Empire.findByIdAndDelete(id);
+
+            if (!result){
+                return res.status(404).json({ message: 'Empire not found' });
+            } else{
+                return res.status(200).send({ message: 'Empire is deleted !' });
+            }
+
+        }catch (err){
+            console.log(err);
+            res.status(500).json({ error: 'Internal server error' });
+        }
+    });
+
+
+
+export default routes
